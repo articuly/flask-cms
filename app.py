c08f82ed542326f2cd8c3625dec730a64b47bd04
carl <luxiaoping@lanou3g.com>
2020-01-12 15:49:49 +0800

    ORM与模型代码

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask, render_template
 from flask import request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///my.db"
+
+db=SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    # 假设有这么一些新闻
-    lists = [
-        {"title": "头条新闻", "intro": "习近平发表了新年演说"},
-        {"title": "新年新闻", "intro": "ETC收费据说费用更高了"},
-        {"title": "过大年了", "intro": "全国互联网公司都在发红包"},
-    ]
-
-
-    return render_template("index.html" , newsLists=lists)
+    return render_template("index.html" )
 
 
 @app.route('/login', methods=['get', 'post'])
@@ -26,14 +24,42 @@ def login():
         print(username, password)
     return render_template("login.html")
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+    realname = db.Column(db.String)
+    sex = db.Column(db.Integer)
+    mylike = db.Column(db.String)
+    city = db.Column(db.String)
+    intro = db.Column(db.String)
 
-
-@app.route("/register")
+@app.route("/register", methods=['get','post'])
 def register():
+    if request.method == "POST":
+        realname = request.form['name']
+        username = request.form['username']
+        password = request.form['password']
+        sex      = request.form['sex']
+        mylike   = '|'.join(request.form.getlist('like'))
+        city     = request.form['city']
+        intro    = request.form['intro']
+        user = User(realname=realname,
+                    username=username,
+                    password=password,
+                    sex=sex,
+                    mylike=mylike,
+                    city=city,
+                    intro=intro)
+
+
+        db.session.add(user)
+        db.session.commit()
+
     return render_template("register.html")
 
 
 @app.context_processor
 def account():
-    username = "luxp"
+    username = None
     return {"username":username}
